a8ac29b4476b53e39f538cb3dd2690187cae715d
Daniel Pullan <dan@3264.uk>
2022-11-14 02:44:40 +0000

    Options are in lists/arrays now
    It made what I'm returning shorter. I'd like to look at putting more into arrays as it looks cleaner.

--- time/main.py
+++ time/main.py
@@ -7,18 +7,9 @@ from flask import Flask, request, render_template, make_response, redirect, url_
 app = Flask(__name__)
 
 
-
-name="Noisy"
-
 @app.route('/')
 def index():
 
-	option1 = "Europe/London"
-	option1name = "London"
-	option2 = "Europe/Dublin"
-	option2name= "dublin"
-
-	
 	option1 = request.cookies.get('option1')
 	option2 = request.cookies.get('option2')
 	option1name = request.cookies.get('option1name')
@@ -43,9 +34,13 @@ def index():
 	option1 = datetime_option1.strftime('%H:%M')
 	option2 = datetime_option2.strftime('%H:%M')
 
+	option1list = [option1, option1name]
+	option2list = [option2, option2name]
 
 
-	return render_template("index.html", uk=uk, eastern=eastern, central=central, onguardforthee=onguardforthee, norge=norge, option1=option1, option1name=option1name, option2=option2, option2name=option2name)
+
+
+	return render_template("index.html", uk=uk, eastern=eastern, central=central, onguardforthee=onguardforthee, norge=norge, option1list=option1list, option2list=option2list)
 
 @app.route('/dos')
 def dos():
@@ -77,10 +72,12 @@ def setcookie():
 
 	   option2 = request.form['option2']
 	   option2name = request.form['option2name']
+
+	   option1list = [option1, option1name]
 	   
-	   resp = make_response(redirect('/'))
-	   resp.set_cookie('option1', option1)
-	   resp.set_cookie('option1name', option1name)
+	   resp = make_response(redirect('/'))	
+	   resp.set_cookie('option1', option1list[0])
+	   resp.set_cookie('option1name', option1list[1])
 	   resp.set_cookie('option2', option2)
 	   resp.set_cookie('option2name', option2name)
    
